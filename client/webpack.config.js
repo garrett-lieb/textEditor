dfff6ec85b8f969a9d69249dc7c6d95359bef902
garrett lieb <liebg08@gmail.com>
2024-03-25 11:19:06 -0700

    webpack.config edits

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Jate',
+        short_name: 'Jate',
+        description: 'A simple text editor',
+        fingetrprints: false,
+        inject: true,
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: 
+              ['@babel/plugin-transform-runtime', 
+              '@babel/plugin-proposal-object-rest-spread', 
+              '@babel/plugin-proposal-class-properties'],
+
+            },
+          },
+        }
       ],
     },
   };
